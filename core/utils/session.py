18843cc4d7c55de12448b62a624dcc271edbc08a
LAPTOP-UFCHV9CV\User <jobnakubphom@gmail.com>
2024-10-08 10:17:40 +0700

    Add insert and getdata

--- core/utils/session.py
+++ core/utils/session.py
@@ -1,35 +1,64 @@
 import os, sys
 sys.path.append(os.path.join(os.getcwd(),"core"))
 
+from token import OP
+from typing import Optional
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 from schema.database.base import Base, Model
 
 from config import SQLALCHEMY_URL
 
+import logging
+from logging.handlers import RotatingFileHandler
 
+logging.getLogger(__name__)
+logging.Formatter()
 
 class ConnectDatabase:
     pass
 
-class CreateSession:
-    def __init__(self, database_url: str, base: any):
-        self.engine = create_engine(database_url)
+class SessionDB:
+    def __init__(self, database_url: str=SQLALCHEMY_URL, base_model: declarative_base=Base):
+        self.engine = create_engine(database_url, echo=True)
         self.session = sessionmaker(bind=self.engine)()
+        self.base_model = base_model
 
     def create_table(self):
-        Base.metadata.create_all(bind=self.engine)
+        self.base_model.metadata.create_all(bind = self.engine)
     
-    def getdata(self, Table):
+    def getdata(self, Table) -> list:
         return self.session.query(Table).all()
+        
+    def getdata_by_condition(self, Table: str, model_name: str):
+        return self.session.query(Table).filter_by(model_name=model_name).all()
         
     def __example(self):
         pass
+    
+    def insert(self,  
+               model_name: Optional[str],
+               route_prefix: Optional[str],
+               version: Optional[int],
+               working_dir: Optional[str],
+               runtime_env: Optional[str],
+               **kwargs
+               ):
+        with self.session as session:
+            new_data = Model(model_name=model_name, route_prefix=route_prefix, version=version, working_dir=working_dir, runtime_env= runtime_env)
+            session.add(new_data)
+            session.commit()
 
 if __name__ == '__main__':
-    Session = CreateSession(SQLALCHEMY_URL, Base)
-    Session.create_table()
-    data = Session.getdata(Table=Model)
-
-    print(data)
+    SessionDB = SessionDB(SQLALCHEMY_URL, Base)
+    SessionDB.create_table()
+    SessionDB.insert(id=2,model_name="test",version='2',route_prefix='/testssss',working_dir='testssss',runtime_env='test')
+    
+    datas = SessionDB.getdata(Table=Model)
+    for data in datas:
+        print(f'Model ID:{data.id}, model_name:{data.model_name}, route_prefix:{data.route_prefix}')
+        
+    condatas = SessionDB.getdata_by_condition(Table=Model,model_name="Hello")
+    for condata in condatas:
+        print(condata.model_name)
